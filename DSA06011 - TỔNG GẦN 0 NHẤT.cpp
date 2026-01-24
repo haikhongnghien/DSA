709ed1bc47984e5fc11fb3787450d3b64d2aea7a
Hải <haibui1362006hd@gmail.com>
2026-01-24 15:39:27 +0700

    Create DSA06011 - TỔNG GẦN 0 NHẤT.cpp

---
+++ DSA06011 - TỔNG GẦN 0 NHẤT.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#define FASTER ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+#define ll long long
+
+int n;
+
+void TestCase() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int &x : a) cin >> x;
+    int ans = a[0] + a[1];
+    for (int i = 0 ; i < n ; i++) {
+        for (int j = i + 1 ; j < n ; j++) {
+            int sum = a[i] + a[j];
+            if (abs(sum) < abs(ans)) {
+                ans = sum;
+            }
+        }
+    }
+    cout << ans << endl;
+}
+
+int main() {
+    
+    FASTER;
+    int t;
+    cin >> t;
+    while(t--) {
+        TestCase();
+    }
+
+    return 0;
+}
