49bc5d3aafed3a51e962e8b14d63579692310be0
Hải <haibui1362006hd@gmail.com>
2026-01-27 16:09:07 +0700

    Create DSA06023 - SẮP XẾP ĐỔI CHỖ TRỰC TIẾP.cpp

---
+++ DSA06023 - SẮP XẾP ĐỔI CHỖ TRỰC TIẾP.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#define FASTER ios_base:: sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+
+int n;
+vector<int> v(105);
+void solve() {
+    int d = 1;
+    for(int i = 0 ; i < n - 1 ; i++) {
+        for(int j = i + 1 ; j < n ; j++) {
+            if(v[i] > v[j]) swap(v[i],v[j]);
+        }
+        cout << "Buoc " << d++ << ": ";
+        for(int j = 0 ; j < n ; j++) {
+            cout << v[j] << ' ';
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+
+    FASTER;
+    cin >> n;
+    for(auto &x : v) cin >> x;
+    solve();
+
+    return 0;
+
+}
