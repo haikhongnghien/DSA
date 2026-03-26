30be393a97250a77db2537eeee81f919ee206edf
Hải <haibui1362006hd@gmail.com>
2026-03-26 22:35:14 +0700

    Create DSA05001 - XÂU CON CHUNG DÀI NHẤT.cpp

---
+++ Dynamic_Programming/DSA05001 - XÂU CON CHUNG DÀI NHẤT.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define FASTER ios_base:: sync_with_stdio(0); cin.tie(0); cout.tie(0);
+const long long MOD = 1e9 + 7;
+#define ll long long
+using namespace std;
+
+void solve() {
+    string s1, s2;
+    cin >> s1 >> s2;
+    int n = s1.size(), m = s2.size();
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+    for(int i = 1 ; i <= n ; i++) {
+        for(int j = 1 ; j <= m ; j++) {
+            if(s1[i - 1] == s2[j - 1]) dp[i][j] = dp[i - 1][j - 1] + 1;
+            else dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+        }
+    }
+    cout << dp[n][m] << endl;
+}
+
+int main() {
+
+    FASTER;
+    int t;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+
+    return 0;
+}
