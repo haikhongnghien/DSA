e89a1b3f67e3971bff55019e39088bc7d566cff7
Hải <haibui1362006hd@gmail.com>
2026-04-07 23:02:57 +0700

    Create DSA08002 - CẤU TRÚC DỮ LIỆU HÀNG ĐỢI 2.cpp

---
+++ Queue/DSA08002 - CẤU TRÚC DỮ LIỆU HÀNG ĐỢI 2.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#define FASTER ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define ll long long
+#define MOD 1e9 + 7;
+#define endl '\n'
+using namespace std;
+
+#include <bits/stdc++.h>
+#define FASTER ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl '\n'
+using namespace std;
+
+int main() {
+
+    FASTER;
+    int t;
+    cin >> t;
+    queue<int> qe;
+    while(t--) {
+        string q;
+        cin >> q;
+        if(q == "PUSH") {
+            int x; cin >> x;
+            qe.push(x);
+        }
+        else if(q == "POP") {
+            if(!qe.empty()) qe.pop();
+        }
+        else if(q == "PRINTFRONT") {
+            if(!qe.empty()) cout << qe.front() << endl;
+            else cout << "NONE" << endl;
+        }
+    }
+
+    return 0;
+}
