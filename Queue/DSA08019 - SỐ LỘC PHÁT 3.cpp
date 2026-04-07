44d3bfd00f26d49de6e27c62595a6051b991b2a3
Hải <haibui1362006hd@gmail.com>
2026-04-07 23:32:17 +0700

    Create DSA08019 - SỐ LỘC PHÁT 3.cpp

---
+++ Queue/DSA08019 - SỐ LỘC PHÁT 3.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#define FASTER ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define ll long long
+#define MOD 1e9 + 7;
+#define endl '\n'
+using namespace std;
+
+void solve() {
+    int n; cin >> n;
+    vector<ll> v;
+    queue<ll> qe;
+    ll t = (ll)pow(10, n);
+    qe.push(6); qe.push(8);
+    while(1) {
+        ll temp = qe.front(); qe.pop();
+        if(temp < t) {
+            v.push_back(temp);
+        }
+        else break;
+        qe.push(temp * 10 + 6);
+        qe.push(temp * 10 + 8);
+    }
+    cout << v.size() << endl;
+    for(int i = v.size() - 1 ; i >= 0 ; i--) cout << v[i] << ' ';
+    cout << endl;
+}
+
+int main() {
+
+    FASTER;
+    int t;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+
+    return 0;
+}
