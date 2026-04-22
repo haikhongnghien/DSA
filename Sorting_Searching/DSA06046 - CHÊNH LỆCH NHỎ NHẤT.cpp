045ffaa2b57301baed3613b3c52b221afbe8d640
Hải <haibui1362006hd@gmail.com>
2026-04-22 17:03:52 +0700

    Create DSA06046 - CHÊNH LỆCH NHỎ NHẤT.cpp

---
+++ Sorting_Searching/DSA06046 - CHÊNH LỆCH NHỎ NHẤT.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#define FASTER ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define ll long long
+#define MOD 1000000007
+#define INF 1e9
+#define endl '\n'
+using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for(auto &it : v) cin >> it;
+    sort(v.begin(), v.end());
+    int ans = INT_MAX;
+    for(int i = 0 ; i < n - 1 ; i++) {
+        ans = min(ans, abs(v[i] - v[i + 1]));
+    }
+    cout << ans << endl;
+}
+
+int main() {
+
+    FASTER;
+    int t;
+    cin >> t;
+    while(t--) solve();
+
+    return 0;
+}
