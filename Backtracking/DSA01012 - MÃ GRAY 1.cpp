cc3d1b263f0f45e0390e6421fc2a5d8faba6866d
Hải <haibui1362006hd@gmail.com>
2026-04-27 14:31:09 +0700

    Update DSA01012 - MÃ GRAY 1.cpp

--- Backtracking/DSA01012 - MÃ GRAY 1.cpp
+++ Backtracking/DSA01012 - MÃ GRAY 1.cpp
@@ -11,7 +11,7 @@ void solve() {
     cin >> n;
     int mask = 1 << n;
     for(int i = 0 ; i < mask ; i++) {
-        //Ma gray thu i = i xor (i >> 1);
+        //gray[k] = binary[k] XOR binary[k+1]
         int gray = i ^ (i >> 1);
         for(int bit = n - 1 ; bit >= 0 ; bit--) {
             cout << ((gray >> bit) & 1);
