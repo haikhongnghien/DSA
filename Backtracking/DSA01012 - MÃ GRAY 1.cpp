59b25b6f15a72ba817b028c553a2488dcf640820
Hải <haibui1362006hd@gmail.com>
2026-04-27 14:28:46 +0700

    Create DSA01012 - MÃ GRAY 1.cpp

---
+++ Backtracking/DSA01012 - MÃ GRAY 1.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#define FASTER ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define ll long long
+#define MOD 1000000007
+#define INF 1e9
+#define endl '\n'
+using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
+    int mask = 1 << n;
+    for(int i = 0 ; i < mask ; i++) {
+        //Ma gray thu i = i xor (i >> 1);
+        int gray = i ^ (i >> 1);
+        for(int bit = n - 1 ; bit >= 0 ; bit--) {
+            cout << ((gray >> bit) & 1);
+        }
+        cout << ' ';
+    }
+    cout << endl;
+}
+
+int main() {
+
+    FASTER;
+    int t;
+    cin >> t;
+    while(t--) solve();
+
+    return 0;
+}
