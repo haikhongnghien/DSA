e9b162e8f2590d994582ab75a819c0350b512e34
Hải <haibui1362006hd@gmail.com>
2026-05-05 22:02:14 +0700

    Create DSA09019 - KIỂM TRA CHU TRÌNH.cpp

---
+++ Graph/DSA09019 - KIỂM TRA CHU TRÌNH.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+#define FASTER ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define ll long long
+#define endl '\n'
+using namespace std;
+
+int V, E;
+int parent[1005], sz[1005];
+bool found;
+
+void Init() {
+    for(int i = 1 ; i <= V ; i++) {
+        parent[i] = i;
+        sz[i] = 1;
+    }
+}
+
+int Find(int u) {
+    if(u == parent[u]) return u;
+    return parent[u] = Find(parent[u]);
+}
+
+bool Union(int u, int v) {
+    u = Find(u); 
+    v = Find(v);
+    if(u == v) return false;
+    if(sz[u] < sz[v]) swap(u, v);
+    parent[v] = u;
+    sz[u] += sz[v];
+    return true;
+}
+
+void solve() {
+    cin >> V >> E;
+    memset(parent, 0, sizeof(parent));
+    memset(sz, 0, sizeof(sz));
+    Init();   
+    found = false;
+    for(int i = 1 ; i <= E ; i++) {
+        int u, v;
+        cin >> u >> v;
+        if(!Union(u,v)) found = true;
+    }
+    cout << (found ? "YES" : "NO") << endl;
+}
+int main() {
+
+    FASTER;
+    int t;
+    cin >> t;
+    while(t--) solve();
+
+    return 0;
+}
