df9e534674279ff7245bf91e3daa0e1285110930
Hải <haibui1362006hd@gmail.com>
2026-05-06 17:21:27 +0700

    Create DSA10006 - CÂY KHUNG CỦA ĐỒ THỊ THEO THUẬT TOÁN DFS.cpp

---
+++ Graph/DSA10006 - CÂY KHUNG CỦA ĐỒ THỊ THEO THUẬT TOÁN DFS.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#define FASTER ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define ll long long
+#define endl '\n'
+using namespace std;
+
+int V, E, root;
+vector<int> ke[1005];
+int visited[1005];
+vector<pair<int,int>> res;
+
+void DFS(int u) {
+    visited[u] = 1;
+    for(auto v : ke[u]) {
+        if(!visited[v]) {
+            res.push_back({u, v});
+            DFS(v);
+        }
+    }
+}
+
+void solve() {
+    cin >> V >> E >> root;
+    for(int i = 1 ; i <= V ; i++) ke[i].clear();
+    memset(visited, 0, sizeof(visited));
+    res.clear();
+    for(int i = 1 ; i <= E ; i++) {
+        int u, v;
+        cin >> u >> v;
+        ke[u].push_back(v);
+        ke[v].push_back(u);
+    }
+    DFS(root);
+    if(res.size() == V - 1) {
+        for(auto [u, v] : res) cout << u << ' ' << v << endl;
+    }
+    else cout << -1 << endl;
+}
+
+int main() {
+
+    FASTER;
+    int t;
+    cin >> t;
+    while(t--) solve();
+
+    return 0;
+}
