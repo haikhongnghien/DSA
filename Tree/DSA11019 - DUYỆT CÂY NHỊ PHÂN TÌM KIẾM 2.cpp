7535fab01c402828703fc5130dfb9eacedc66eb2
Hải <haibui1362006hd@gmail.com>
2026-05-12 09:08:28 +0700

    Create DSA11019 - DUYỆT CÂY NHỊ PHÂN TÌM KIẾM 2.cpp

---
+++ Tree/DSA11019 - DUYỆT CÂY NHỊ PHÂN TÌM KIẾM 2.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#define FASTER ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl '\n'
+using namespace std;
+
+struct Node{
+    int data;
+    Node *left, *right;
+    Node(int x) {
+        data = x;
+        left = right = NULL;
+    }
+};
+
+Node* Insert(Node *root, int value) {
+    if(root == NULL) return new Node(value);
+    if(root -> data > value) {
+        root -> left = Insert(root -> left, value);
+    } 
+    else root -> right = Insert(root -> right, value);
+    return root;
+}
+
+void Postorder(Node *root) {
+    if(root == NULL) return;
+    Postorder(root -> left);
+    Postorder(root -> right);
+    cout << root -> data << ' ';
+}
+
+int main() {
+
+    FASTER;
+    int t;
+    cin >> t;
+    while(t--) {
+        int n;
+        cin >> n;
+        Node *root = NULL;
+        for(int i = 0 ; i < n ; i++) {
+            int x; cin >> x;
+            root = Insert(root, x);
+        }
+        Postorder(root);
+        cout << endl;
+    }
+
+    return 0;
+}
