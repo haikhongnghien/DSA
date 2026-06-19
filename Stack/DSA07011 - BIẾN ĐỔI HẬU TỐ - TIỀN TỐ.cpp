52e47be7e3e6e7d1340c9ea73b08706a7a1c06a8
Hải <haibui1362006hd@gmail.com>
2026-06-19 20:29:53 +0700

    Create DSA07011 - BIẾN ĐỔI HẬU TỐ - TIỀN TỐ.cpp

---
+++ Stack/DSA07011 - BIẾN ĐỔI HẬU TỐ - TIỀN TỐ.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define FASTER ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define ll long long
+#define MOD 1000000007
+#define endl '\n'
+using namespace std;
+
+int t;
+string s;
+
+int main() {
+    FASTER;
+    cin >> t;
+    while(t--) {
+        cin >> s;
+        stack<string> st;
+        for(int i = 0 ; i < s.size() ; i++) {
+            if(isalpha(s[i])) st.push(string(1, s[i]));
+            else {
+                string x = st.top(); st.pop();
+                string y = st.top(); st.pop();
+                st.push(s[i] + y + x);
+            }
+        }
+        cout << st.top() << endl;
+    }
+    return 0;
+}
