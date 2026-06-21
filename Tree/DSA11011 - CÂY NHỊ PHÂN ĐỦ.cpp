717e496dd6915cfebe7fdb91982480b396f9b0f2
Hải <haibui1362006hd@gmail.com>
2026-06-21 14:30:42 +0700

    Create DSA11011 - CÂY NHỊ PHÂN ĐỦ.cpp

---
+++ Tree/DSA11011 - CÂY NHỊ PHÂN ĐỦ.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+#define FASTER ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define ll long long
+#define endl '\n'
+using namespace std;
+
+int t, n, u, v, mn, mx;
+char c;
+bool ok;
+struct Node{
+	int data;
+	Node *left, *right;
+	Node(int x) {
+		data = x;
+		left = right = NULL;
+	}
+};
+
+void MakeNode(Node *root, int u, int v, char c) {
+	if(c == 'L') root -> left = new Node(v);
+	else root -> right = new Node(v);
+}
+
+void Insert(Node *root, int u, int v, char c) {
+	if(root == NULL) return;
+	if(root -> data == u) {
+		MakeNode(root, u, v, c);
+		return;
+	}
+	Insert(root -> left, u, v, c);
+	Insert(root -> right, u, v, c);
+}
+
+void check(Node *root) {
+	if(root == NULL) return;
+	if((root -> left == NULL) != (root -> right == NULL)) {
+		ok = false;
+		return;
+	}
+	check(root -> left);
+	check(root -> right);
+}
+
+int main() {
+    FASTER;
+	cin >> t;
+	while(t--) {
+		cin >> n;
+		Node *root = NULL;
+		for(int i = 1 ; i <= n ; i++) {
+			cin >> u >> v >> c;
+			if(root == NULL) {
+				root = new Node(u);
+				MakeNode(root, u, v, c);
+			}
+			else Insert(root, u, v, c);
+		}
+		ok = true;
+		check(root);
+		cout << (ok ? 1 : 0) << endl;
+	}
+    return 0;
+}
